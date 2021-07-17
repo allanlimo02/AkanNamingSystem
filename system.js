d9cd6f62dc213de902badff363a014987f283847
Allanlimo02 <allan.limo@student.moringaschool.com>
2021-07-17 08:58:00 +0300

    Added data varification

---
+++ system.js
@@ -0,0 +1,41 @@
+// var username=document.getElementById('username');
+// var date= document.getElementById('date');
+// var month=document.getElementById('month');
+// var year= document.getElementById('year');
+// var gender=document.getElementById('gender');
+
+// function validator(){
+//     if(username.value==''){
+//         alert('Username cant be blank');
+//         username.focus();
+        
+//     }
+//     if (date.value==''||date.value>=31){
+//         alert('Enter s vslid Date value');
+//         date.focus();
+        
+//     }
+//     if (month==''||month.value>12){
+//         alert('Input a valid value for month');
+//         month.focus();
+//         return false;
+//     }
+//     if(year==''|| year.length!==4){
+//         alert('Input a valid year');
+//         year.focus();
+//         return false;
+//     }
+//     if (gender==''){
+//         alert(' Gender cant be blank');
+//         gender.focus();
+//         return false;
+
+//     }
+//     console.lg(document.writeElementById('year'))
+    
+    
+// }
+
+console.log("Hello ");
+var m= new.Date();
+
